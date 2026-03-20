cd4d279c7719d7b425128b28b80e493e285407fa
Biter01 <sebastian.lamplmair@gmail.com>
2026-03-20 01:45:26 +0100

    #49 [6h] bugfix falscher Pfad in GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/teaching-2026.ss.prse.braeuer.team5/', // <--- dein Repo-Name
   plugins: [react()],
 })
