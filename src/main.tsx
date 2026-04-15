eab83bdb2fc5ca7d52aec1beedddb28d374ef60a
Sebastian <79320619+Biter01@users.noreply.github.com>
2026-04-15 10:03:17 +0200

    Switch from BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // basename sorgt dafür, dass alle Links automatisch den Präfix aus vite.config bekommen
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
